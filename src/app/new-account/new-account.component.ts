13a3fa55baa1b0f90ed5a41206b88d2ff0fef00c
michalsadovski <michal.sadovski@gmail.com>
2022-08-04 15:41:29 +0200

    Installing Angular Materials, setting modules, creating key components, placing content in new-account.component

---
+++ src/app/new-account/new-account.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-account',
+  templateUrl: './new-account.component.html',
+  styleUrls: ['./new-account.component.css']
+})
+export class NewAccountComponent implements OnInit {
+  status = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
